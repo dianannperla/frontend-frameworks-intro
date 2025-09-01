a9faefe21fc9018ade19edda51c3f56bb66e453d
dianannperla <waafirmelyza@hotmail.com>
2025-09-01 16:39:13 +0100

    Fix alert message typo for clarity

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const sections = document.querySelectorAll('section');
     sections.forEach(section => {
         section.addEventListener('click', () => {
-            alert(`You clicked on the introduction section about ${section.querySelector('h2').innerText}.`);
+            alert(`You clicked on the section about ${section.querySelector('h2').innerText}.`);
         });
     });
 });
